4da5a0a8ec8fd07b92d7c4ff16c87b96b7bf9013
jessgascoigne <jessagascoigne@gmail.com>
2022-08-18 16:27:15 -0500

    Added Dictionary component with a search form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import Footer from "./Footer";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>📖 Dictionary App</h1>
+      <Dictionary />
       <Footer />
     </div>
   );
---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  const [word, setWord] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${word}`);
+  }
+
+  function handleWordSearch(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Search for a word..."
+          autoFocus="on"
+          onChange={handleWordSearch}
+        />
+      </form>
+    </div>
+  );
+}
