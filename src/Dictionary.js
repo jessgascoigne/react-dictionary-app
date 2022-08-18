913c952e221bd4c8af56de0eca602d3ee5ba776c
jessgascoigne <jessagascoigne@gmail.com>
2022-08-18 16:30:14 -0500

    Added search form reset

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,6 +6,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${word}`);
+    event.target.reset();
   }
 
   function handleWordSearch(event) {
