cc92ce9333ff1353f29a53f51cd239c8159b3013
jessgascoigne <jessagascoigne@gmail.com>
2022-08-18 16:12:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./Footer";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>📖 Dictionary App</h1>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/jessgascoigne/react-dictionary-app"
+        target="_blank"
+        rel="noreferrer"
+        className="github-repo-link"
+      >
+        open source code
+      </a>{" "}
+      by jess gascoigne
+    </div>
+  );
+}
