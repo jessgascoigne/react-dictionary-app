995e04fdc3bbbf727997e1f93fbf084b0d1b2247
jessgascoigne <jessagascoigne@gmail.com>
2022-08-19 16:34:47 -0500

    Moved phonetics audio & text from component to functions within results component

--- src/Results.js
+++ src/Results.js
@@ -1,21 +1,40 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetics from "./Phonetics";
 
 import "./Results.css";
 
 export default function Results(props) {
+  function showPhoneticsAudio() {
+    if (props.results.phonetics[0].audio.length > 0) {
+      return (
+        <button>
+          <a
+            href={props.results.phonetics[0].audio}
+            target="_blank"
+            rel="noreferrer"
+          >
+            listen
+          </a>
+        </button>
+      );
+    } else {
+      return null;
+    }
+  }
+
+  function showPhoneticsText() {
+    if (props.results.phonetics[0].text.length > 0) {
+      return <h5>{props.results.phonetics[0].text}</h5>;
+    }
+  }
+
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        {props.results.phonetics.map(function (phonetics, index) {
-          return (
-            <div key={index}>
-              <Phonetics phonetics={phonetics} />
-            </div>
-          );
-        })}
+        <hr />
+        <div>{showPhoneticsAudio()}</div>
+        <div>{showPhoneticsText()}</div>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
