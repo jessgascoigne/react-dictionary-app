c8c666f69427f7c3211d0036e48b774c95a71fb7
jessgascoigne <jessagascoigne@gmail.com>
2022-08-19 16:35:37 -0500

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
-        if (index < 3) {
-          return (
-            <div key={index}>
-              <h4>{definition.definition}</h4>
-              <br />
-              <p>{definition.example}</p>
-              <Synonyms synonyms={definition.synonyms} />
-            </div>
-          );
-        } else {
-          return null;
-        }
+        return (
+          <div key={index}>
+            <h4>
+              {index + 1}
+              {". "}
+              {definition.definition}
+            </h4>
+            <p>{definition.example}</p>
+            <Synonyms synonyms={definition.synonyms} />
+          </div>
+        );
       })}
+      <hr />
     </div>
   );
 }
