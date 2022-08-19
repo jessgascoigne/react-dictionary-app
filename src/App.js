4d1ef8430e74b63d8da385b0795f86e17276fa09
jessgascoigne <jessagascoigne@gmail.com>
2022-08-18 21:05:20 -0500

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>📖 Dictionary App</h1>
-      <Dictionary />
+      <div className="body">
+        <h1>📖 Dictionary App</h1>
+        <Dictionary />
+      </div>
       <Footer />
     </div>
   );
