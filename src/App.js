047a6d98fc606fb4cdf6d4587bd87b93f3a31376
jessgascoigne <jessagascoigne@gmail.com>
2022-08-31 11:21:14 -0500

    Added bootstrap & moved header to Dictionary.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Footer from "./Footer";
 
 import "./App.css";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function App() {
   return (
     <div className="App">
       <div className="body">
-        <h1>What word do you want to look up?</h1>
         <Dictionary defaultWord="code" />
       </div>
       <Footer />
