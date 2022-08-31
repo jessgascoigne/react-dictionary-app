5ab4fc637e20b14f56e237b03bf0a31fad8c9f1d
jessgascoigne <jessagascoigne@gmail.com>
2022-08-31 11:20:56 -0500

    Added photos section, book illustration, & search header from App.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
+import Book from "./images/book.svg";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   const [word, setWord] = useState(props.defaultWord);
   const [results, setResults] = useState("");
+  const [photos, setPhotos] = useState("");
   const [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     setLoaded(true);
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "563492ad6f9170000100000130e4a5af89de4d6487477fdf034036db";
+    let perPage = "9";
+    const pexelsApiUrl = `https://api.pexels.com/v1/search?query=${word}&per_page=${perPage}`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -30,15 +44,25 @@ export default function Dictionary(props) {
   if (loaded) {
     return (
       <div className="Dictionary">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="search"
-            defaultValue={word}
-            autoFocus="on"
-            onChange={handleWordSearch}
-          />
-        </form>
+        <div className="row">
+          <div className="col-6 search">
+            <h1>What word do you want to look up?</h1>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="search"
+                defaultValue={word}
+                autoFocus="on"
+                onChange={handleWordSearch}
+              />
+            </form>
+          </div>
+
+          <div className="col-6">
+            <img src={Book} alt="girl reading" className="float-end" />
+          </div>
+        </div>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
